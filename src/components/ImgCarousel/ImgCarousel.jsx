c9e089743562f9c02aae05904cc6e9cbb25c2e3c
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2026-01-26 21:51:22 -0600

    WORKING: ImgCarousel.jsx and ImgCard.jsx
    I am making an image carousel that displays all of the
    fortnite's messages of the day. I successfully setup a
    fetch request to the API to get the MOTD's, but the
    cards are deconstructed, so I plan to dynamically render the
    long img carousel part as an unordered list of NewsCards that
    have their own style associated with them, similar to how I
    did it back during the memory game react project. Remember that
    array items need to have a key property associated with them in order to
    keep their place.

--- src/components/ImgCarousel/ImgCarousel.jsx
+++ src/components/ImgCarousel/ImgCarousel.jsx
@@ -1,14 +1,60 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import Styles from "./ImgCarousel.module.css";
 
 function ImgCarousel() {
-    const [fortniteImgArr, setFortniteImgArr] = useState([]);
+  // data, loading, and error states for data fetching
+  const [fortniteDataArr, setFortniteDataArr] = useState(null);
+  const [fetchError, setFetchError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    try {
+      const processFortniteData = (fortniteJSON) => {
+        // extract and copy the MOTD object array to a temp and return that
+        const tempArr = [...fortniteJSON.data.br.motds];
 
-    return(
-        <div>Img goes here</div>
-    );
+        return tempArr;
+      };
 
+      const fetchFortniteData = async () => {
+        // fetch today's fortnite news
+        const fortniteAPIResponse = await fetch(
+          "https://fortnite-api.com/v2/news",
+        );
+        const fortniteJSON = await fortniteAPIResponse.json();
+        const fortniteMOTDS = processFortniteData(fortniteJSON);
+        setFortniteDataArr(fortniteMOTDS);
+      };
 
+      fetchFortniteData();
+    } catch (error) {
+      setFetchError(error.message);
+      setFortniteDataArr(null);
+    } finally {
+      // if fetch went well, then change loading to false to trigger final render upon re-render of data
+      setLoading(false);
+      console.log("done");
+    }
+  }, []);
+
+  //   loading state, display if API data is still being fetched
+  if (loading) {
+    return <div>News loading...</div>;
+  }
+
+//   error state, display if API fetch was unsuccessful (error code 400 or above)
+if (fetchError) {
+    return <div>A network error/fetching error occured.</div>
 }
 
+
+// data state, display when API data is fully usable
+  return(
+    <div className={Styles.carouselContainer}>
+        <button>{"<"}</button>
+        <ul>images here</ul>
+        <button>{">"}</button>
+    </div>
+  );
+}
 
 export default ImgCarousel;
---
+++ src/components/ImgCarousel/NewsCard.jsx
@@ -0,0 +1,6 @@
+
+function NewsCard() {
+
+}
+
+export default NewsCard;
