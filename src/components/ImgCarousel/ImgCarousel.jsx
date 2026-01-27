23e1b64d060fba648aa8e14728bf8892a1f0321c
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2026-01-27 15:33:06 -0600

    WORKING: finished left and right ImgCarousel button styling and structure. Need to implement MOTD sliding functionality
    Finished how the general MOTD is going to look centered on the screen. Added
    a linear gradient background to the entire HTML to have a default background.
    Need to code in the button functionality with the fortnite MOTD array moving.
    Maybe implement a small bubble to keep track of which image they are currently on.

--- src/components/ImgCarousel/ImgCarousel.jsx
+++ src/components/ImgCarousel/ImgCarousel.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import Styles from "./ImgCarousel.module.css";
+import NewsCard from "./NewsCard";
 
 function ImgCarousel() {
   // data, loading, and error states for data fetching
-  const [fortniteDataArr, setFortniteDataArr] = useState(null);
+  const [fortniteDataArr, setFortniteDataArr] = useState([]);
   const [fetchError, setFetchError] = useState(null);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
@@ -32,7 +33,6 @@ function ImgCarousel() {
     } finally {
       // if fetch went well, then change loading to false to trigger final render upon re-render of data
       setLoading(false);
-      console.log("done");
     }
   }, []);
 
@@ -41,18 +41,35 @@ function ImgCarousel() {
     return <div>News loading...</div>;
   }
 
-//   error state, display if API fetch was unsuccessful (error code 400 or above)
-if (fetchError) {
-    return <div>A network error/fetching error occured.</div>
-}
+  //   error state, display if API fetch was unsuccessful (error code 400 or above)
+  if (fetchError) {
+    return <div>A network error/fetching error occured.</div>;
+  }
 
+  //   utility functions for data state render
+  const makeNewsCards = () => {
+    const tempCardArr = fortniteDataArr.map((news) => {
+      return (
+        <NewsCard
+          key={crypto.randomUUID()}
+          newsImg={news.image}
+          newsDescription={news.body}
+          newsTitle={news.title}
+        />
+      );
+    });
 
-// data state, display when API data is fully usable
-  return(
-    <div className={Styles.carouselContainer}>
-        <button>{"<"}</button>
-        <ul>images here</ul>
-        <button>{">"}</button>
+    return tempCardArr;
+  };
+
+  // data state, display when API data is fully usable
+  return (
+    <div className={Styles.uiContainer}>
+      <button className={Styles.left}>{"<"}</button>
+      <div className={Styles.carouselContainer}>
+        <ul className={Styles.carouselList}>{makeNewsCards()}</ul>
+      </div>
+      <button className={Styles.right}>{">"}</button>
     </div>
   );
 }
--- src/components/ImgCarousel/NewsCard.jsx
+++ src/components/ImgCarousel/NewsCard.jsx
@@ -1,6 +1,16 @@
+import Styles from "./NewsCard.module.css";
 
-function NewsCard() {
+function NewsCard({ newsImg, newsDescription, newsTitle }) {
+  return (
+    <li className={Styles.newsItem}>
+      <img className={Styles.newsImg} src={newsImg} alt="fortnite news image" />
 
+      <div className={Styles.newsContent}>
+        <h1 className={Styles.newsTitle}>{newsTitle}</h1>
+        <p className={Styles.newsDesc}>{newsDescription}</p>
+      </div>
+    </li>
+  );
 }
 
 export default NewsCard;
