c9b3c700767844c7b850e4946ceb567ef0d3456d
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2026-01-28 01:27:54 -0600

    TEST: making image carousel tests to check button behavior

---
+++ src/tests/ImgCarousel tests/ImgCarousel.test.jsx
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+describe("Image Carousel buttons make the carousel move", () => {
+// TODO: make tests testing behavior of carousel buttons
+});
