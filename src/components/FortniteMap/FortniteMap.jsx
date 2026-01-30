f19f7b22c01bac129a944a46cb8eed8e48b41f0d
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2026-01-30 11:12:44 -0600

    FEAT: finished map toggling logic and toggle button style
    Map swaps between the POI and blank map when button is toggled.
    Also changed the font-family o the buttons to be the imported
    fortnite font.

--- src/components/FortniteMap/FortniteMap.jsx
+++ src/components/FortniteMap/FortniteMap.jsx
@@ -7,10 +7,16 @@ function FortniteMap() {
   const [loading, setLoading] = useState(true);
   const [fetchError, setFetchError] = useState(null);
 
+  // useState for the current toggled fortnite map
+  const [selectedMap, setSelectedMap] = useState(null);
+
   // fetch the current map image from fortnite API
   useState(() => {
     const processMapData = (fortniteJSON) => {
       const tempMapObj = { ...fortniteJSON.data.images };
+
+      // set the selected map to initially be the POI map
+      setSelectedMap(tempMapObj.pois);
 
       return tempMapObj;
     };
@@ -28,6 +34,8 @@ function FortniteMap() {
 
         const fortniteMapObj = processMapData(APIdata);
 
+        console.log(fortniteMapObj);
+
         setFortniteMapData(fortniteMapObj);
       } catch (error) {
         setFortniteMapData(null);
@@ -42,7 +50,9 @@ function FortniteMap() {
 
   // API error render
   if (fetchError) {
-    return <h1 className={Styles.error}>A network error/fetching error occured.</h1>;
+    return (
+      <h1 className={Styles.error}>A network error/fetching error occured.</h1>
+    );
   }
 
   //   Data still loading render
@@ -50,12 +60,36 @@ function FortniteMap() {
     return <h1 className={Styles.loading}>Map data loading...</h1>;
   }
 
-  //   data render
 
+  // utility functions for data render
+
+  const toggleHandler = () => {
+    if(selectedMap === fortniteMapData.pois) {
+      // if the current selected map is the POI map, swap to blank map
+      return setSelectedMap(fortniteMapData.blank)
+    }
+
+    // otherwise, swap to POI map
+    return setSelectedMap(fortniteMapData.pois);
+  };
+  
+
+  //   data render
   // TODO: a map object is now assigned to fortniteMapData, have a button that toggles hiding the POI or display POIS on the map
   return (
     <section>
       <h1 className={Styles.mapTitle}>Map</h1>
+      <div className={Styles.buttonFlexContainer}>
+       { (selectedMap === fortniteMapData.pois) ? <button className={Styles.toggleButton} onClick={toggleHandler}>Hide POI </button> : <button className={Styles.toggleButton} onClick={toggleHandler}>Show POI</button>  }
+      </div>
+      <div className={Styles.mapFlexContainer}>
+        <img
+          src={selectedMap}
+          alt="Current fortnite season map"
+          className={Styles.fortniteMap}
+        />
+      </div>
+      
     </section>
   );
 }
