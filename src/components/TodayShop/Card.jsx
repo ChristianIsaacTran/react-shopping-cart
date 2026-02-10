083ad3197ba2f74935fd9bbfa347c55a7600aa92
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2026-02-10 16:21:14 -0600

    FIX: swapped dynamic render statement to render item .icon first instead of .feature first
    attempting to render the image found in brItem[0].image.feature instead of
    .icon caused some items to display incorrect images (specifically any item
    that was an instrument). Swapping them forces the card to use the .icon first, THEN
    the .feature if .icon was not found.

--- src/components/TodayShop/Card.jsx
+++ src/components/TodayShop/Card.jsx
@@ -4,69 +4,70 @@ import vbucksLogo from "../../assets/images/fortniteVBucks.png";
 function Card({ cryptoKey, itemData }) {
   console.log(itemData);
 
-//   returns rarity styling depending on the item rarity
+  //   returns rarity styling depending on the item rarity
   const checkRarity = (isBundle = false) => {
-    
-    // check if the item object is a bundle. If it is, have the rarity banner say bundle 
-    if(isBundle) {
-        return (<div className={Styles.rarityLine + " " + Styles.bundle }>BUNDLE</div>);
+    // check if the item object is a bundle. If it is, have the rarity banner say bundle
+    if (isBundle) {
+      return (
+        <div className={Styles.rarityLine + " " + Styles.bundle}>BUNDLE</div>
+      );
     }
-    
-    
+
     // if normal item, then check rarity
     // converts string rarity to lowercase for easier comparison
     const rarity = itemData.brItems[0].rarity.displayValue.toLowerCase();
 
-    if(rarity === "legendary") {
-        // legendary rarity check
-        return (<div className={Styles.rarityLine + " " + Styles.legendary}></div>);
-    } else if(rarity === "epic") {
-        // epic rarity check
-        return (<div className={Styles.rarityLine + " " + Styles.epic}></div>);
-    }else if(rarity === "rare") {
-        // rare rarity check
-        return (<div className={Styles.rarityLine + " " + Styles.rare}></div>);
-    }else if(rarity === "uncommon") {
-        // uncommon rarity check
-        return (<div className={Styles.rarityLine + " " + Styles.uncommon}></div>);
-    }else if(rarity === "common") {
-        // common rarity check
-        return (<div className={Styles.rarityLine + " " + Styles.common}></div>);
-    }else if(rarity === "icon series") {
-        // icon series rarity check
-        return (<div className={Styles.rarityLine + " " + Styles.icon}></div>);
-    } else if(rarity === "marvel series") {
-        // marvel series rarity check
-        return (<div className={Styles.rarityLine + " " + Styles.marvel}></div>);
-    } else if(rarity === "gaming legends series") {
-        // gaming legends series rarity check
-        return (<div className={Styles.rarityLine + " " + Styles.gamingLegends}></div>);
-    } else if(rarity === "star wars series") {
-        // star wars series rarity check
-        return (<div className={Styles.rarityLine + " " + Styles.starWars}></div>);
-    } else if(rarity === "dc series") {
-        // DC series rarity check
-        return (<div className={Styles.rarityLine + " " + Styles.dc}></div>);
-    } else if(rarity === "slurp series") {
-        // slurp series rarity check
-        return (<div className={Styles.rarityLine + " " + Styles.slurp}></div>);
-    } else if(rarity === "shadow series") {
-        // shadow series rarity check
-        return (<div className={Styles.rarityLine + " " + Styles.shadow}></div>);
-    } else if(rarity === "lava series") {
-        // lava series rarity check
-        return (<div className={Styles.rarityLine + " " + Styles.lava}></div>);
-    } else if(rarity === "frozen series") {
-        // frozen series rarity check
-        return (<div className={Styles.rarityLine + " " + Styles.frozen}></div>);
-    } else if(rarity === "dark series") {
-        // dark series rarity check
-        return (<div className={Styles.rarityLine + " " + Styles.dark}></div>);
+    if (rarity === "legendary") {
+      // legendary rarity check
+      return <div className={Styles.rarityLine + " " + Styles.legendary}></div>;
+    } else if (rarity === "epic") {
+      // epic rarity check
+      return <div className={Styles.rarityLine + " " + Styles.epic}></div>;
+    } else if (rarity === "rare") {
+      // rare rarity check
+      return <div className={Styles.rarityLine + " " + Styles.rare}></div>;
+    } else if (rarity === "uncommon") {
+      // uncommon rarity check
+      return <div className={Styles.rarityLine + " " + Styles.uncommon}></div>;
+    } else if (rarity === "common") {
+      // common rarity check
+      return <div className={Styles.rarityLine + " " + Styles.common}></div>;
+    } else if (rarity === "icon series") {
+      // icon series rarity check
+      return <div className={Styles.rarityLine + " " + Styles.icon}></div>;
+    } else if (rarity === "marvel series") {
+      // marvel series rarity check
+      return <div className={Styles.rarityLine + " " + Styles.marvel}></div>;
+    } else if (rarity === "gaming legends series") {
+      // gaming legends series rarity check
+      return (
+        <div className={Styles.rarityLine + " " + Styles.gamingLegends}></div>
+      );
+    } else if (rarity === "star wars series") {
+      // star wars series rarity check
+      return <div className={Styles.rarityLine + " " + Styles.starWars}></div>;
+    } else if (rarity === "dc series") {
+      // DC series rarity check
+      return <div className={Styles.rarityLine + " " + Styles.dc}></div>;
+    } else if (rarity === "slurp series") {
+      // slurp series rarity check
+      return <div className={Styles.rarityLine + " " + Styles.slurp}></div>;
+    } else if (rarity === "shadow series") {
+      // shadow series rarity check
+      return <div className={Styles.rarityLine + " " + Styles.shadow}></div>;
+    } else if (rarity === "lava series") {
+      // lava series rarity check
+      return <div className={Styles.rarityLine + " " + Styles.lava}></div>;
+    } else if (rarity === "frozen series") {
+      // frozen series rarity check
+      return <div className={Styles.rarityLine + " " + Styles.frozen}></div>;
+    } else if (rarity === "dark series") {
+      // dark series rarity check
+      return <div className={Styles.rarityLine + " " + Styles.dark}></div>;
     }
 
-
-    return (<div>rarity not found...</div>);
-  }
+    return <div>rarity not found...</div>;
+  };
 
   // checks if the item is a bundle, regular item, jamtrack, etc. and renders the respective information
   const renderItemType = () => {
@@ -127,8 +128,8 @@ function Card({ cryptoKey, itemData }) {
         <>
           <img
             src={
-              itemData.brItems[0].images.featured ||
-              itemData.brItems[0].images.icon
+              itemData.brItems[0].images.icon ||
+              itemData.brItems[0].images.featured
             }
             alt="item image"
             className={Styles.cardImg}
