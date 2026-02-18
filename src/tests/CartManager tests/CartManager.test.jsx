1d14733b69e623ed8bd8d1a9ba5d85ea91f8ed88
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2026-02-17 19:15:47 -0600

    INIT and TEST: created test file for CartManager.jsx, need test logic
    Initialized test folder and file. Need to code the actual user interaction
    logic to test if the CartManager.jsx displays the cart correctly.

---
+++ src/tests/CartManager tests/CartManager.test.jsx
@@ -0,0 +1,8 @@
+import {describe, expect, it, vi, afterEach} from "vitest";
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event"
+import CartManager from "../../components/CartManager/CartManager";
+
+describe("CartManager displays cart correctly on cart page", () => {
+    
+});
